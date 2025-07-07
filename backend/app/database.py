6a1c4314d44a0e93f3621e556e03ba643f3e8b44
Jas7l <jasilhasil1@gmail.com>
2025-07-07 15:20:16 +0300

    Added sync container

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
@@ -13,7 +14,22 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
+# For API
 def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+        db.commit()
+    except:
+        db.rollback()
+        raise
+    finally:
+        db.close()
+
+
+# For sync_runner
+@contextmanager
+def context_db():
     db = SessionLocal()
     try:
         yield db
