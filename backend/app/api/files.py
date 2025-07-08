37e1691f4e883ab08af8ce4bdc345eda3354223f
Jas7l <jasilhasil1@gmail.com>
2025-07-08 13:13:01 +0300

    update router

--- backend/app/api/files.py
+++ backend/app/api/files.py
@@ -10,7 +10,7 @@ router = APIRouter()
 
 
 # Route for syncing the database with the local storage
-@router.post("/files/sync")
+@router.post("/api/files/sync")
 async def sync_files(background_tasks: BackgroundTasks):
     background_tasks.add_task(FileManager.sync_storage_and_db)
     return {"detail": "Sync started in background"}
@@ -18,7 +18,7 @@ async def sync_files(background_tasks: BackgroundTasks):
 
 # Route to get all files or filter files by path
 # URL example: /files?path=my-path
-@router.get("/files", response_model=List[FileRead])
+@router.get("/api/files", response_model=List[FileRead])
 def get_files(path: Optional[str] = Query(None)):
     files = FileManager.get_all_files(path)
     if not files:
@@ -27,19 +27,19 @@ def get_files(path: Optional[str] = Query(None)):
 
 
 # Route to get file by ID
-@router.get("/files/{file_id}", response_model=FileRead)
+@router.get("/api/files/{file_id}", response_model=FileRead)
 def get_file(file_id: int):
     return FileManager.get_file_by_id(file_id)
 
 
 # Route to download a file
-@router.get("/files/{file_id}/download")
+@router.get("/api/files/{file_id}/download")
 def download_file(file_id: int):
     return FileManager.download_file(file_id)
 
 
 # Route to upload a file
-@router.post("/files", response_model=FileRead)
+@router.post("/api/files", response_model=FileRead)
 def upload_file(
     uploaded_file: UploadFile = File(...),
     path: Optional[str] = Form(""),
@@ -49,12 +49,12 @@ def upload_file(
 
 
 # Route to update a file
-@router.patch("/files/{file_id}", response_model=FileRead)
+@router.patch("/api/files/{file_id}", response_model=FileRead)
 def update_file(file_id: int, file_data: FileUpdate):
     return FileManager.update_file(file_id, file_data)
 
 
 # Route to delete a file
-@router.delete("/files/{file_id}", response_model=FileRead)
+@router.delete("/api/files/{file_id}", response_model=FileRead)
 def delete_file(file_id: int):
     return FileManager.delete_file(file_id)
